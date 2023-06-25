92943e7834477eda996ad032cc8c0b53a68ba7f0
arthurbg-14 <arthurbraga_99@hotmail.com>
2023-06-25 11:39:13 -0300

    foo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ function Contador(){
     setContador(contador + 1)
   }
 
-  return <div className="bg-gray-700 m-5 p-4 rounded-md flex flex-col items-center">
+  return <div className="bg-gray-700 border-2 border-gray-800 m-5 p-4 rounded-md flex flex-col items-center">
     <div className="bg-gray-500 p-4 rounded">{contador}</div>
     <button className="bg-gray-500 w-6 h-6 flex items-center justify-center mt-3 rounded hover:bg-gray-300" onClick={adicionarAoContador}>+</button>
   </div>
