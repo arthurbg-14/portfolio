9416217df986e1e3cd82f405f43f8851d959aa70
arthurbg-14 <arthurbraga_99@hotmail.com>
2023-06-25 11:43:38 -0300

    foo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,8 @@ export default function Home() {
   return (
     <>
       <div className="w-[100vw] h-[100vh] flex items-center justify-center flex-col">
-        Minha pagina!
-        <Contador/>
+        <div className="absolute top[25%]">Minha Pagina!</div>
       </div>
     </>
   )
-}
-
-function Contador(){
-  const [contador, setContador] = useState(0)
-
-  function adicionarAoContador(){
-    setContador(contador + 1)
-  }
-
-  return <div className="bg-gray-700 border-2 m-5 p-4 rounded-md flex flex-col items-center">
-    <div className="bg-gray-500 p-4 rounded">{contador}</div>
-    <button className="bg-gray-500 w-6 h-6 flex items-center justify-center mt-3 rounded hover:bg-gray-300" onClick={adicionarAoContador}>++</button>
-  </div>
 }
