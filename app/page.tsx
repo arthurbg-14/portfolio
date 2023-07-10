3438d78d8ddde15ed6d0612132ae172a86026514
arthur <arthurbraga_99@hotmail.com>
2023-07-10 00:30:24 -0300

    refactoring code

--- app/page.tsx
+++ app/page.tsx
@@ -10,30 +10,21 @@ export default function Home() {
   const router = useRouter()
   let [isVisible, setVisible] = useState(true)
 
-  function goToFrontend(e: any) {
-    e.preventDefault()
-    setVisible(false)
+  function goTo(target: string) {
     setTimeout(() => {
-      router.push("/frontend")
-    },300)
-  }
-  function goToBackend(e: any) {
-    e.preventDefault()
-    setVisible(false)
-    setTimeout(() => {
-      router.push("/backend")
+      router.push(target)
     },300)
   }
 
   return (
     <Spawn isVisible={isVisible}>
       <div className="mt-[5%] w-[100vw] flex justify-center text-3xl">Arthur | Dev Fullstack!</div>
-      <Link href="/frontend" onClick={goToFrontend}>
+      <Link href="/frontend" onClick={() => {goTo("/frontend")}}>
         <div className="group flex items-center justify-center absolute w-[50vw] h-[100vh] left-0 top-0">
           <h1 className="text-2xl group-hover:text-4xl ease-in-out duration-300 pb-1 px-3 border-b-[2px]">Front-End</h1>
         </div>
       </Link>
-      <Link href="/backend" onClick={goToBackend}>
+      <Link href="/backend" onClick={() => {goTo("/backend")}}>
         <div className="group flex items-center justify-center absolute w-[50vw] h-[100vh] left-[50vw] top-0">
           <h1 className="text-2xl group-hover:text-4xl ease-in-out duration-300 pb-1 px-3 border-b-[2px]">Back-End</h1>
         </div>
